a1e2a17bdde694f86e7f349cf029f797238ea116
Mohhamadaminn <mohamad4aminm@gmail.com>
2025-06-13 00:33:51 +0330

    synchronization templates with same styles

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,13 @@ class CustomUserCreationForm(UserCreationForm):
   class Meta:
       model = CustomUser
       fields = ['username', ]
+
+  def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-700'
+            })
   
 
 
--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content',]
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-700'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-700',
+                'rows': 5
+            }),
+        }
 
 class ProfileForm(forms.ModelForm):
     class Meta:
